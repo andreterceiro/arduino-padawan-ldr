7f44ee983880126628e01ed47e8ab881359096ae
terceiro.com.br <andre@terceiro.com.br>
2019-03-05 21:36:37 -0300

    Add files via upload

---
+++ ldr.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A1, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println(analogRead(A1));  
+  delay(1000);
+}
